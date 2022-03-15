1b84baff16c449064a52a07055ff8f1f3008e900
Thiago <thiago099carvalho@gmail.com>
2022-03-15 01:54:26 -0300

    feat: UnBArqDsw2021-2/2021.2_G5_Garimpei#100 add request validation
    Co-authored-by: Mateus Gomes <mateusgomes.nascimento21@gmail.com>

--- src/user/dto/update-user.dto.ts
+++ src/user/dto/update-user.dto.ts
@@ -1,9 +1,18 @@
 import { PartialType } from '@nestjs/mapped-types';
+import { IsString, Length } from 'class-validator';
 import { CreateUserDto } from './create-user.dto';
 
 export class UpdateUserDto extends PartialType(CreateUserDto) {
+  @IsString()
   name?: string;
+
+  @IsString()
   nickname?: string;
+
+  @IsString()
   password?: string;
+
+  @IsString()
+  @Length(11, 11)
   phone?: string;
 }
