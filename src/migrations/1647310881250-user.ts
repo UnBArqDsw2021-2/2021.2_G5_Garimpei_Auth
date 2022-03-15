c6e62c82c95770de217db87b4008d0eb666f9d7e
Thiago <thiago099carvalho@gmail.com>
2022-03-14 23:57:15 -0300

    feat: UnBArqDsw2021-2/2021.2_G5_Garimpei#100 add user migration and delete specs
    Co-authored-by: Mateus Gomes <mateusgomes.nascimento21@gmail.com>

---
+++ src/migrations/1647310881250-user.ts
@@ -0,0 +1,14 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class user1647310881250 implements MigrationInterface {
+    name = 'user1647310881250'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "user" ADD CONSTRAINT "UQ_e12875dfb3b1d92d7d7c5377e22" UNIQUE ("email")`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "user" DROP CONSTRAINT "UQ_e12875dfb3b1d92d7d7c5377e22"`);
+    }
+
+}
