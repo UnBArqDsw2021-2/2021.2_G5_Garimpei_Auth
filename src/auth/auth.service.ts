3347f36c1b30fd6e0f6340002499c42baa10f3e3
Mateus Gomes <mateusgomes.nascimento21@gmail.com>
2022-03-18 01:08:00 -0300

    feat: UnBArqDsw2021-2/2021.2_G5_Garimpei#103 add auth token to login endpoint
    Co-authored-by: Vinicius Saturnino <viniciussaturnino78@gmail.com>
    Co-authored-by: Victor Gonçalves <victor.eng.unb@gmail.com>

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,10 +2,22 @@ import { Injectable } from '@nestjs/common';
 import { User } from 'src/user/entities/user.entity';
 import { UserService } from 'src/user/user.service';
 import { compareSync } from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly userService: UserService) {}
+  constructor(
+    private readonly userService: UserService,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  async login(user) {
+    const payload = { sub: user.id, email: user.email };
+
+    return {
+      token: this.jwtService.sign(payload),
+    };
+  }
 
   async validateUser(email: string, password: string) {
     let user: User;
