b7f1729a3ff61cabb552dae78a9621fd0ae495eb
VictorJorgeFGA <victor.eng.unb@gmail.com>
2022-04-08 23:05:22 -0300

    fix: UnBArqDsw2021-2/2021.2_G5_Garimpei#161 fix community migration

--- src/migrations/1649463826471-community.ts
+++ src/migrations/1649463826471-community.ts
@@ -1,10 +1,10 @@
 import {MigrationInterface, QueryRunner} from "typeorm";
 
-export class community1649386696146 implements MigrationInterface {
-    name = 'community1649386696146'
+export class community1649463826471 implements MigrationInterface {
+    name = 'community1649463826471'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE TABLE "community" ("id" SERIAL NOT NULL, "name" character varying NOT NULL, "description" character varying NOT NULL, "phone" character varying NOT NULL, "location" character varying NOT NULL, "city" character varying NOT NULL, "cep" character varying NOT NULL, "latitude" integer NOT NULL, "email" character varying NOT NULL, "banner" character varying NOT NULL, "photo" character varying NOT NULL, "createdAt" TIMESTAMP NOT NULL DEFAULT ('now'::text)::timestamp(6) with time zone, "updatedAt" TIMESTAMP NOT NULL DEFAULT ('now'::text)::timestamp(6) with time zone, "adminId" integer, CONSTRAINT "REL_c82fe9d81fd4c5acc6c98b40fe" UNIQUE ("adminId"), CONSTRAINT "PK_cae794115a383328e8923de4193" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`CREATE TABLE "community" ("id" SERIAL NOT NULL, "name" character varying NOT NULL, "email" character varying NOT NULL, "city" character varying NOT NULL, "phone" character varying NOT NULL, "description" character varying, "location" character varying, "cep" character varying, "latitude" integer, "banner" character varying, "photo" character varying, "createdAt" TIMESTAMP NOT NULL DEFAULT ('now'::text)::timestamp(6) with time zone, "updatedAt" TIMESTAMP NOT NULL DEFAULT ('now'::text)::timestamp(6) with time zone, "adminId" integer NOT NULL, CONSTRAINT "REL_c82fe9d81fd4c5acc6c98b40fe" UNIQUE ("adminId"), CONSTRAINT "PK_cae794115a383328e8923de4193" PRIMARY KEY ("id"))`);
         await queryRunner.query(`ALTER TABLE "user" ADD "communityId" integer`);
         await queryRunner.query(`ALTER TABLE "user" ADD CONSTRAINT "FK_9da479465ea980eca3c3d5f4cea" FOREIGN KEY ("communityId") REFERENCES "community"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
         await queryRunner.query(`ALTER TABLE "community" ADD CONSTRAINT "FK_c82fe9d81fd4c5acc6c98b40fe2" FOREIGN KEY ("adminId") REFERENCES "user"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
