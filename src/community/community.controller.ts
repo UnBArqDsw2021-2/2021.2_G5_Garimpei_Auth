8040410a8c1762f619fb8177f8e401238d67251d
VictorJorgeFGA <victor.eng.unb@gmail.com>
2022-04-11 19:05:00 -0300

    feat: UnBArqDsw2021-2/2021.2_G5_Garimpei#162 add more endpoints to community controller
    Co-authored-by: Denys Rogeres <dennysrogeres@gmail.com>
    Co-authored-by: Juliana Valle <juju.pvg@gmail.com>
    Co-authored-by: Paulo Victor <paulovmel@gmail.com>

--- src/community/community.controller.ts
+++ src/community/community.controller.ts
@@ -1,4 +1,7 @@
-import { Body, Controller, Delete, Get, Param, Post, Put } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Param, Post, Put, UseGuards, Req } from "@nestjs/common";
+import { AuthGuard } from "@nestjs/passport";
+import { Request } from "express";
+import { UserReferenceDto } from "src/user/dto/user-reference.dto";
 import { CommunityService } from "./community.service";
 import { CreateCommunityDto } from "./dto/create-community.dto";
 import { UpdateCommunityDto } from "./dto/update-community.dto";
@@ -8,8 +11,9 @@ export class CommunityController {
     constructor(private readonly communityService: CommunityService) {}
 
     @Post()
-    create(@Body() createCommunityDto: CreateCommunityDto) {
-        return this.communityService.create(createCommunityDto);
+    @UseGuards(AuthGuard('jwt'))
+    create(@Req() request: Request, @Body() createCommunityDto: CreateCommunityDto) {
+        return this.communityService.create(createCommunityDto, request.user);
     }
 
     @Get()
@@ -23,12 +27,32 @@ export class CommunityController {
     }
 
     @Put(':id')
-    update(@Param('id') id: string, @Body() updateCommunityDto: UpdateCommunityDto) {
-        return this.communityService.update(+id, updateCommunityDto);
+    @UseGuards(AuthGuard('jwt'))
+    update(@Req() request: Request, @Param('id') id: string, @Body() updateCommunityDto: UpdateCommunityDto) {
+        return this.communityService.update(+id, updateCommunityDto, request.user);
     }
 
     @Delete(':id')
-    remove(@Param('id') id: string) {
-        return this.communityService.remove(+id);
+    @UseGuards(AuthGuard('jwt'))
+    remove(@Req() request: Request,@Param('id') id: string) {
+        return this.communityService.remove(+id, request.user);
+    }
+
+    @Post(':id/add-user')
+    @UseGuards(AuthGuard('jwt'))
+    addUser(@Req() request: Request, @Param('id') id: string, @Body() userReferenceDto: UserReferenceDto) {
+        return this.communityService.addUser(+id, userReferenceDto, request.user);
+    }
+
+    @Post(':id/remove-user')
+    @UseGuards(AuthGuard('jwt'))
+    removeUser(@Req() request: Request, @Param('id') id: string, @Body() userReferenceDto: UserReferenceDto) {
+        return this.communityService.removeUser(+id, userReferenceDto, request.user);
+    }
+
+    @Get(':id/members')
+    @UseGuards(AuthGuard('jwt'))
+    members(@Param('id') id: string) {
+        return this.communityService.members(+id);
     }
 }
